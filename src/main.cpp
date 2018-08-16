ac481c355080fd7a80e10b562d6afb17f406ba05
ye546 <anton_bosse@hotmail.com>
2018-08-16 12:32:59 +0200

    Fixed a string bug
    string bug fixed

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,6 @@ void RandomizeNewRotation(vector &all_maps, vector &old_maps);
 void LoadMaps(std::string path, vector &maps);
 void save(vector &v);
 
-
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	INITCOMMONCONTROLSEX icex;
@@ -78,14 +77,14 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpCmdLine, in
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	HFONT hf;
+	HFONT hf, sHf;
+	long lfHeight;
 	HDC hdc;
 	switch (msg)
 	{
 		case WM_CREATE:
-		{
 			//create a font
-			long lfHeight;
+			
 			hdc = GetDC(NULL);
 			lfHeight = -MulDiv(12, GetDeviceCaps(hdc, LOGPIXELSY), 72);
 			ReleaseDC(NULL, hdc);
@@ -93,16 +92,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 			//buttons
 			field1 = CreateWindowEx(WS_EX_CLIENTEDGE, "Edit", "available_maps.txt", WS_CHILD | WS_VISIBLE, 10, 10, 150, 25, hWnd, NULL, hInst, (LPVOID)lParam);	//new rotation
-			field2 = CreateWindowEx(WS_EX_CLIENTEDGE, "Edit", "old_rotation.txt", WS_CHILD | WS_VISIBLE, 10, 40, 150, 25, hWnd, NULL, hInst, (LPVOID)lParam);	//old rotation
-
+			field2 = CreateWindowEx(WS_EX_CLIENTEDGE, "Edit", "excluded maps.txt", WS_CHILD | WS_VISIBLE, 10, 40, 150, 25, hWnd, NULL, hInst, (LPVOID)lParam);	//old rotation
+			
 			button1 = CreateWindowEx(NULL, "Button", "Load", WS_CHILD | WS_VISIBLE, 180, 10, 50, 20, hWnd, NULL, hInst, (LPVOID)lParam);	//load the txt files
 			button2 = CreateWindowEx(NULL, "Button", "Save", WS_CHILD | WS_VISIBLE, 180, 40, 50, 20, hWnd, NULL, hInst, (LPVOID)lParam);	//write to file and save
 
 			static1 = CreateWindowEx(NULL, "Static", "New", WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER, 300, 10, 90, 20, hWnd, NULL, hInst, (LPVOID)lParam);	//new rotation
-			static2 = CreateWindowEx(NULL, "Static", "Old", WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER, 550, 10, 90, 20, hWnd, NULL, hInst, (LPVOID)lParam);	//old rotation
+			static2 = CreateWindowEx(NULL, "Static", "Excluded", WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER, 550, 10, 90, 20, hWnd, NULL, hInst, (LPVOID)lParam);	//old rotation
 
 			textarea1 = CreateWindowEx(NULL, "Static", "", WS_CHILD | WS_VISIBLE | WS_BORDER, 250, 40, 200, 400, hWnd, NULL, hInst, (LPVOID)lParam);	//new rotation
-			textarea2 = CreateWindowEx(NULL, "Static", "", WS_CHILD | WS_VISIBLE | WS_BORDER, 500, 40, 200, 400, hWnd, NULL, hInst, (LPVOID)lParam);	//old rotation
+			textarea2 = CreateWindowEx(NULL, "Edit", "", WS_CHILD | WS_VISIBLE | WS_BORDER | WS_VSCROLL | ES_MULTILINE, 500, 40, 200, 400, hWnd, NULL, hInst, (LPVOID)lParam);	//old rotation
+
+			//to allow scrolling if you add a lot of maps
+			//that you want to exclude
+			SendMessage(textarea2,EM_SETREADONLY, 1, 0);
 
 			//set font for every window created
 			SendMessage(field1, WM_SETFONT, (WPARAM)hf, TRUE);
@@ -113,10 +116,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			SendMessage(static2, WM_SETFONT, (WPARAM)hf, TRUE);
 			SendMessage(textarea1, WM_SETFONT, (WPARAM)hf, TRUE);
 			SendMessage(textarea2, WM_SETFONT, (WPARAM)hf, TRUE);
-
-
-		}
-		break;
+			break;
 		case WM_COMMAND:
 			{
 				//arrays to get the filenames
@@ -157,24 +157,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 					//and the new rotation
 					SetWindowTextA(textarea1, tmp1.c_str());
 					SetWindowTextA(textarea2, tmp2.c_str());
-
+					
 				}
 				if (msg == WM_COMMAND && (HWND)lParam == button2) {
 					vector new_rotation;
-					
-					std::ifstream file;
-					
-					//read the rotation from the temporary file
-					file.open("holder1.txt");
-					
-					//fill the vector like usual
-					std::string tmp;
-					while (std::getline(file, tmp))
-						new_rotation.push_back(tmp);
-
 					//call the save function to save the rotation
 					save(new_rotation);
-					MessageBox(hWnd, "Successfully saved the rotation to 'New Rotation.txt'", "Success!", MB_ICONINFORMATION);
+					MessageBox(hWnd, "Successfully saved the rotation to \"New Rotation.txt\"", "Success!", MB_ICONINFORMATION);
 				}
 			}
 			break;
@@ -186,6 +175,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			//remove holders used for temp storage
 			std::remove("holder1.txt");
 			std::remove("holder2.txt");
+			std::remove("holder3.txt");
 			break;
 		default:
 			return DefWindowProc(hWnd, msg, wParam, lParam);
@@ -193,31 +183,39 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 }
 
 void save(vector &v) {
-	std::fstream file("New_Rotation.txt", std::fstream::app | std::fstream::out);
+	std::ofstream file("New Rotation.txt", std::fstream::app);
 	//check if the file is empty or not
 	//if it is we don't printf a \n
 	//this is simply to make it look better
 	if (file.is_open()) {
-		file.seekg(0, std::ios::end);
-		size_t size = file.tellg();
+		file.seekp(0, std::ios::end);
+		size_t size = file.tellp();
 		if (size > 0)
 			file << '\n';
 	}
+	
+	std::ifstream file2;
+	//read the rotation from the temporary file
+	file2.open("holder3.txt");
+
+	//fill the vector like usual
+	std::string tmp;
+	while (std::getline(file2, tmp))
+		v.push_back(tmp);
+
+	file2.close();
 
 	//print this to initiate the list
-	file << "gametype" << " " << "zom" << " " << "map" << " ";
+	file << "gametype" << " zom" << " map ";
 
 	for (int i = 0; i < v.size(); i++)
 	{
-		//take reference because othwerise
-		//we check if it's equal to the value
-		//whats on that index
-		if (&v[i] == &v.back()) {
-			file << v[i];
-		}
-		else
-			file << v[i] << " " << "map" << " "; //if we're not on the last index of the vector then print this to the file
+		file << v[i];
 	}
+
+	//ISSUE NOTCED: THE READING FROM THE FILE IS READ AS 1 STRING, THIS IS NOT INTENDED.
+
+	file.close();
 }
 
 void LoadMaps(std::string path, vector &maps) {
@@ -269,12 +267,24 @@ void RandomizeNewRotation(vector &all_maps, vector &old_maps) {
 	//in wndproc and write to them
 	std::ofstream file1("holder1.txt");
 	std::ofstream file2("holder2.txt");
+	std::ofstream file3("holder3.txt");
 
 	//the writing to the file
-	for (int i = 0; i < new_rotation.size(); i++) {
-		file1 << new_rotation[i] << ' ';
+
+	//for displaying purposes
+	for (int i = 0; i < new_rotation.size(); i++) { 
+		file1 << new_rotation[i] << '\r';
 	}
+	//for displaying purposes
 	for (int i = 0; i < old_maps.size(); i++) {
-		file2 << old_maps[i] << ' ';
+		file2 << old_maps[i] << '\r';
 	}
+	//actual rotation format
+	for (int i = 0; i < new_rotation.size(); i++) {
+		if (&new_rotation[i] == &new_rotation.back()) {
+			file3 << new_rotation[i];
+		}
+		else
+			file3 << new_rotation[i] << " map ";
+	}		
 }
